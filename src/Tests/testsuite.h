19e79f52e427d69d9edb8ed55898e02569737965
dtodd <dtodd@75907e46-c664-0410-ae3d-e599167a7a88>
2009-10-22 01:03:51 +0000

    Added directory of tests and associated "correct" results. The golden subdirectory includes the officially accepted results. The gamma_test.suite will be used to test these results using a python script.

---
+++ src/Tests/testsuite.h
@@ -0,0 +1,23 @@
+// This file contains interfaces to a set of gamma tests
+#include <gamma.h>
+#include <string>
+
+class GammaTest
+
+{
+
+public:
+
+static int iso_test();
+
+static int fid_test(std::string & sysfile);
+
+static int spinecho_test(std::string & sysfile);
+
+static int spinecho_realpulse_test(std::string & sysfile, std::string & pulse180file);
+
+static int press_realpulses_test(std::string & sysfile, std::string & pulse180file);
+
+static int fid_exchange_test(std::string & sysfile);
+
+};
