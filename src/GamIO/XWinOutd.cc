3802526415f5e62759bd9e8e52cdfe50b1583c48
maer <maer@75907e46-c664-0410-ae3d-e599167a7a88>
2014-12-03 07:23:33 +0000

    Corrected mistake when assigning initial value to time_t longtime. Should be a long and not a pointer.

--- src/GamIO/XWinOutd.cc
+++ src/GamIO/XWinOutd.cc
@@ -130,7 +130,7 @@ void XWinOutd::SetDefaults()
   _JCAMPDX  = "5.0";
   _DATATYPE = "Parameter Values";
   time_t longtime;              // Need a time structure
-  longtime = NULL;        //
+  longtime = 0;        //
 
 #ifdef _MSC_VER
 	struct tm newtime;
@@ -272,7 +272,7 @@ int XWinOutd::write(int warn) const
   ofstr << nn << "JCAMPDX= "  << _JCAMPDX << "\n";
   ofstr << nn << "DATATYPE= " << _DATATYPE << "\n";
   time_t longtime;				// Need a time structure
-  longtime = NULL;
+  longtime = 0;
 
 #ifdef _MSC_VER
 	struct tm newtime;
